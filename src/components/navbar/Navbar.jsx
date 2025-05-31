4e5d8fad783c09bdab53959e5ba2d43200eb983f
bobur <bobur@gmail.com>
2025-05-31 21:37:24 +0500

    added dark mode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,11 +5,24 @@ import avatar from "../../images/Avatar.svg"
 import "./navbar.css"
 
 function Navbar() {
+
+  const changeTheme = (e) => {
+    e.preventDefault()
+    const htmlTag = document.documentElement; 
+    const currentTheme = htmlTag.getAttribute("data-theme");
+
+    if (currentTheme === "dark") {
+      htmlTag.removeAttribute("data-theme");
+    } else {
+      htmlTag.setAttribute("data-theme", "dark");
+    }
+  };
+  
   return (
     <div className='navbar'>
       <Link className="logo_link" to="/"><img src={logo} alt="site-logo" width="103px" height="103px"/></Link>
       <div className="avatar_mainDiv">
-      <button className="mode_btn"><img className="mode_icon" src={darkmode} alt="" /></button>
+      <button onClick={changeTheme}  className="mode_btn"><img className="mode_icon" src={darkmode} alt="" /></button>
           <hr className="navbar_hr"/>
           <Link className="navbar_ava_wrapper">
                <img className="navbar_ava" src={avatar} alt="" />
